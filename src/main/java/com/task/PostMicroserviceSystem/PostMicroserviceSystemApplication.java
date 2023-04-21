bac968ba6cdcd897673df0f4b09f93f79d97cd4a
Santanu Barua <santanu>
2023-04-21 19:32:28 +0200

    Initial commit

---
+++ src/main/java/com/task/PostMicroserviceSystem/PostMicroserviceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.task.PostMicroserviceSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostMicroserviceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostMicroserviceSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/PostMicroserviceSystem/PostMicroserviceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.PostMicroserviceSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostMicroserviceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
