9b93ee41d82dc46b7e2e1dd98cb633be83b13b0b
Santanu26 <cushantanu@gmail.com>
2023-06-05 13:22:33 +0200

    Refactor code

--- docker/post-db-init/init-post-db.sql
+++ docker/post-db-init/init-post-db.sql
@@ -7,5 +7,5 @@ CREATE TABLE IF NOT EXISTS post (
   text_of_post VARCHAR(255) NOT NULL,
   posted_at DATE NOT NULL
 );
-insert into post(author_id, text_of_post, posted_at)
-values (1,'Hi this post service', '2023-04-25');
+insert into post(author_id, text_of_post, posted_at) values (1,'Hi this post service', '2023-04-25');
+insert into post(author_id, text_of_post, posted_at) values (2,'Hi this post service from user 2', '2023-04-25');
